55566059221d8fb174946c0f0e26a6040c233607
Yang Vang <lanceyvang@gmail.com>
2018-06-15 12:55:34 -0400

    finished footernav and cardview components

--- App.js
+++ App.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import FooterNav from './Components/FooterNav';
+import CardView from './Components/CardView';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <CardView />
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// <View style={styles.container}>
+//   <Text>Open up App.js to start working on your app!</Text>
+//   <Text>Changes you make will automatically reload.</Text>
+//   <Text>Shake your phone to open the developer menu.</Text>
+// </View>
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ Components/FooterNav.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Container, Header, Content, Footer, FooterTab, Button, Icon } from 'native-base';
+export default class FooterTabsIconExample extends Component {
+  render() {
+    return (
+      <Container>
+        <Header />
+        <Content />
+        <Footer>
+          <FooterTab>
+            <Button>
+              <Icon name="apps" />
+            </Button>
+            <Button>
+              <Icon name="camera" />
+            </Button>
+            <Button active>
+              <Icon active name="navigate" />
+            </Button>
+            <Button>
+              <Icon name="person" />
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
+    );
+  }
+}
