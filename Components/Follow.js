99b8357b9e42961cde64af52b0e35028614f06a0
Yang Vang <lanceyvang@gmail.com>
2018-06-18 00:48:59 -0400

    last commit for the night

--- Components/Follow.js
+++ Components/Follow.js
@@ -1,20 +1,46 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { ListItem } from 'react-native-elements'
+import { Header } from 'native-base';
 
+const list = [
+  {
+    name: 'Mikey Chen',
+    avatarUrl: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Youtuber'
+  },
+  {
+    name: 'Mark Weins',
+    avatarUrl: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'Youtuber'
+  },
+  // ... // more items
+]
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: "center",
+//     justifyContent: "center"
+//   }
+// });
 
-const Test = () => (
-  <View style={styles.container}>
-    <Text>Follow People Here</Text>
+const Follow = () => {
+  return (
+    <View >
+      <Header />
+      {
+        list.map((l, i) => (
+          <ListItem
+            key={i}
+            leftAvatar={{ source: { uri: l.avatarUrl } }}
+            title={l.name}
+            subtitle={l.subtitle}
+          />
+        ))
+      }
+    </View >
+  )
+};
 
-  </View>
-);
-
-export default Test;
+export default Follow;
